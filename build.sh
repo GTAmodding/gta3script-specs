d84a28ef0dfaf72818829ec5d5c5de1a4296d7a1
Denilson das Mercês Amorim <dma_2012@hotmail.com>
2018-11-24 22:07:56 -0300

    Update deploy script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pandoc --toc --standalone -M title="GTA3script Specification" --number-sections --css=pandoc.css core/*.md -o core.html
+pandoc --toc --standalone -M title="GTA3script Specification" --number-sections --css="css/pandoc.css" core/*.md -o core.html
--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,10 @@ set -e
 git clone https://github.com/gtamodding/gta3script-specs.git /tmp/gta3script-specs
 cd /tmp/gta3script-specs
 ./build.sh
+mkdir build
+cp *.html build/
 git checkout gh-pages
+cp build/*.html ./ && rm -r build
 git add *.html
 git commit -m "Deploy"
 git push origin gh-pages
