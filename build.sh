faff547bbabfa19ab8227bd586c6f59e6962dd15
Denilson das Mercês Amorim <dma_2012@hotmail.com>
2018-12-02 15:20:19 -0300

    Move so called DMA extensions out of the core

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
-#!/bin/sh
-asciidoctor -v -a linkcss -a docinfo=shared-head -a docinfodir="$(pwd)" core/index.adoc -o core.html -r ./tools/asciidoctor-grammar-preprocessor.rb
+#!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
+
+build_html()
+{
+    asciidoctor $1 -o $2 -v -a linkcss -a docinfo=shared-head -a docinfodir=$BASEDIR -r "$BASEDIR/tools/asciidoctor-grammar-preprocessor.rb"
+}
+
+build_html "$BASEDIR/core/index.adoc" "$BASEDIR/core.html"
+build_html "$BASEDIR/dma/index.adoc" "$BASEDIR/dma.html"
