4a06b1d5f4f1fbed0a2da0219354162cc5ec7189
Denilson das Mercês Amorim <dma_2012@hotmail.com>
2020-04-07 01:43:44 -0300

    Update deploy script to deploy DMA extensions

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,8 @@ set -e
 git clone https://github.com/gtamodding/gta3script-specs.git /tmp/gta3script-specs
 cd /tmp/gta3script-specs
 ./build.sh
-mkdir build
-cp *.html build/
-cp *.css build/
 git checkout gh-pages
-cp build/*.html ./
-cp build/*.css ./
-rm -r build
-git add *.html *.css
+find . -type f \( -name '*.html' -or -name '*.css' \) -exec git add {} \;
 git commit -m "Deploy"
 git push origin gh-pages
 cd /tmp && rm -rf gta3script-specs
