112f97762d99da785a00bb6b028b23235121ee5b
Denilson das Mercês Amorim <dma_2012@hotmail.com>
2020-04-08 19:40:07 -0300

    Fix deprecated usage of footnoteref

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ build_html()
     asciidoctor $1 -o $2 -v -a linkcss -a docinfo=shared-head -a docinfodir=$BASEDIR -r "$BASEDIR/tools/asciidoctor-grammar-preprocessor.rb"
 }
 
+# The generated HTML files **must** be at BASEDIR
 build_html "$BASEDIR/core/index.adoc" "$BASEDIR/core.html"
-build_html "$BASEDIR/dma/gta3/index.adoc" "$BASEDIR/dma/gta3.html"
+build_html "$BASEDIR/dma/gta3/index.adoc" "$BASEDIR/gta3.html"
 build_html "$BASEDIR/index.adoc" "$BASEDIR/index.html"
--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@ set -e
 git clone https://github.com/gtamodding/gta3script-specs.git /tmp/gta3script-specs
 cd /tmp/gta3script-specs
 ./build.sh
+mkdir build build/dma build/extensions
+cp *.html *.css build/
 git checkout gh-pages
-find . -type f \( -name '*.html' -or -name '*.css' \) -exec git add {} \;
-git rm docinfo.html
+cp build/*.html build/*.css ./
+rm -r build docinfo.html
+git add .
 git commit -m "Deploy"
 git push origin gh-pages
 cd /tmp && rm -rf gta3script-specs
